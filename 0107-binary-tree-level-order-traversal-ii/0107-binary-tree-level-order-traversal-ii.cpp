72144eaa01728c3f394ccc23e8381e4cd0745783
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2023-01-22 11:57:14 +0530

    Time: 4 ms (74.76%), Space: 12.5 MB (67.95%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -12,32 +12,45 @@
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>> res;
-        vector<TreeNode *> q;
         if(!root){
-            return res;
+            return {};
         }
-        TreeNode *dummy=new TreeNode(-1001);
-        q.insert(q.begin(),root);
+        int control=1;
+        vector<TreeNode *> q1,q2;
+        vector<vector<int>> res;
         vector<int> cL;
-        q.insert(q.end(),dummy);
-        while(q.size()!=1){
-            // int a=q[0]->val;
-            cL.push_back(q[0]->val);
-            if(q[0]->left){
-                q.insert(q.end(),q[0]->left);
+        q1.insert(q1.begin(),root);
+        while(q1.size()!=0||q2.size()!=0){
+            if(control==1){
+                if(q1[0]->left){
+                    q2.insert(q2.end(),q1[0]->left);
+                }
+                if(q1[0]->right){
+                    q2.insert(q2.end(),q1[0]->right);
+                }
+                cL.push_back(q1[0]->val);
+                q1.erase(q1.begin());
+                if(q1.size()==0){
+                    control=2;
+                    res.push_back(cL);
+                    cL={};
+                }
             }
-            if(q[0]->right){
-               q.insert(q.end(),q[0]->right);
+            else{
+                if(q2[0]->left){
+                    q1.insert(q1.end(),q2[0]->left);
+                }
+                if(q2[0]->right){
+                    q1.insert(q1.end(),q2[0]->right);
+                }
+                cL.push_back(q2[0]->val);
+                q2.erase(q2.begin());
+                if(q2.size()==0){
+                    control=1;
+                    res.push_back(cL);
+                    cL={};
+                }
             }
-            q.erase(q.begin());
-            if(q[0]==dummy){
-                q.erase(q.begin());
-                res.push_back(cL);
-                cL={};
-                q.insert(q.end(),dummy);
-            }
-            // res.push_back(cL);
         }
         reverse(res.begin(),res.end());
         return res;
