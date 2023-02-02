1ccf4d438212b2cddf3aa518b827ea87d762cc24
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2023-02-02 18:39:45 +0530

    Added solution - LeetHub

---
+++ Nearly sorted - GFG/nearly-sorted.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+	priority_queue <int, vector<int>, greater<int> > pq;
+	vector<int> res;
+	for(int i=0;i<num;++i){
+	    pq.push(arr[i]);
+	   // cout<<arr[i]<<endl;
+	    if(i>=K){
+	        res.push_back(pq.top());
+	        pq.pop();
+	    }
+	}
+	while(!pq.empty()){
+	   // cout<<pq.top()<<endl;
+	    res.push_back(pq.top());
+	    pq.pop();
+	}
+        // Your code here
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
