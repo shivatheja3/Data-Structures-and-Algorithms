a0e0bff414a8d50fca30cfd6b94bc481c83e0e1e
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2023-04-01 16:40:19 +0530

    Time: 139 ms (5.54%), Space: 27.1 MB (11.98%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    bool check(vector<int> &nums){
+        // for(int i=0;i<nums.size();++i){
+        //     cout<<nums[i]<<" ";
+        // }
+        // cout<<endl;
+        if(nums[0]>nums[nums.size()-1]){
+            return false;
+        }
+        for(int i=0;i<(nums.size()-1);++i){
+            if(nums[i]>nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkPossibility(vector<int>& nums) {
+        if(nums.size()<=2){
+            return true;
+        }
+        for(int i=0;i<nums.size();++i){
+            if(i==0){
+                int x=-100001;
+                // if(nums[i]>nums[i+1]){
+                    x=nums[i];
+                    nums[i]=nums[i+1];
+                // }
+                if(check(nums)){
+                    return true;
+                }
+                // if(x!=-100001){
+                    nums[i]=x;
+                // }
+            }
+            else if(i>0&&i<(nums.size()-1)){
+                int x=-100001;
+                // if(nums[i]<nums[i-1]){
+                    x=nums[i];
+                    nums[i]=nums[i-1];
+                // }
+                if(check(nums)){
+                    return true;
+                }
+                // if(x!=-100001){
+                    nums[i]=x;
+                // }
+            }
+            else{
+                int x=-100001;
+                if(nums[i]<nums[i-1]){
+                    x=nums[i];
+                    nums[i]=nums[i-1];
+                }
+                if(check(nums)){
+                    return true;
+                }
+                if(x!=-100001){
+                    nums[i]=x;
+                }
+            }
+        }
+        return false;
+    }
+};
