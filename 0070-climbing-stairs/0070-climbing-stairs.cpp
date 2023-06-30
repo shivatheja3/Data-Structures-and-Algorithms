34c8d30667516c3dfa3e47b8a27a7faad963f36b
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2023-06-30 22:27:41 +0530

    Time: 4 ms (13.02%), Space: 6.2 MB (14.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int a1=1,a2=1,a3=1;
-        for(int i=2;i<=n;++i){
-            a3=a1+a2;
-            a1=a2;
-            a2=a3;
+    int func(int n,int i,vector<int> &dp){
+        if(i==n)
+            return 1;
+        if(i>n)
+            return 0;
+        if(dp[i]!=-1){
+            return dp[i]; 
         }
-        return a3;
+        int a=0;
+        if(i+1<=n)
+            a=dp[i+1];
+        int b=0;
+        if(i+2<=n){
+            b=dp[i+2];
+        }
+        // if(i+1<=n&&dp[i+1]==-1)
+            a=func(n,i+1,dp);
+        // if(i+2<=n&&dp[i+2]==-1)
+            b=func(n,i+2,dp);
+        dp[i]=a+b;
+        return dp[i];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return func(n,0,dp);
     }
 };
