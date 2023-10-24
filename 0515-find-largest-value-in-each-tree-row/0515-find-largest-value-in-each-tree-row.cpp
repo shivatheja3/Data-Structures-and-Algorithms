18932e6627c31b7589c7d464791466c78fecc15e
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2023-10-24 10:01:43 +0530

    Time: 16 ms (9.36%), Space: 22.3 MB (72.02%) - LeetHub

---
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        if(!root){
+            return {};
+        }
+        int control=1;
+        vector<TreeNode *> q1,q2;
+        vector<int> res;
+        vector<int> cL;
+        q1.insert(q1.begin(),root);
+        while(q1.size()!=0||q2.size()!=0){
+            if(control==1){
+                if(q1[0]->left){
+                    q2.insert(q2.end(),q1[0]->left);
+                }
+                if(q1[0]->right){
+                    q2.insert(q2.end(),q1[0]->right);
+                }
+                cL.push_back(q1[0]->val);
+                q1.erase(q1.begin());
+                if(q1.size()==0){
+                    control=2;
+                    res.push_back(*max_element(cL.begin(),cL.end()));
+                    cL={};
+                }
+            }
+            else{
+                if(q2[0]->left){
+                    q1.insert(q1.end(),q2[0]->left);
+                }
+                if(q2[0]->right){
+                    q1.insert(q1.end(),q2[0]->right);
+                }
+                cL.push_back(q2[0]->val);
+                q2.erase(q2.begin());
+                if(q2.size()==0){
+                    control=1;
+                    res.push_back(*max_element(cL.begin(),cL.end()));
+                    cL={};
+                }
+            }
+        }
+        return res;
+    }
+};
