d9b9f6b7372d410857a631ef97c58bfa167d8fb9
Shiva Theja <74763721+shivatheja3@users.noreply.github.com>
2024-02-05 00:18:05 +0530

    Time: 46 ms (69.92%), Space: 51 MB (15.40%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size()==1) return true;
-        else if(nums.size()==2){
-            if(nums[0]==0)
-                return false;
-            else
-                return true;
+        int m1=nums[0],i=0;
+        for(i=0;i<nums.size();++i){
+            if(m1>=nums.size()-1) return 1;
+            if((nums[i]+m1)==i) return 0;
+            m1=max(m1,nums[i]+i);
         }
-        for(int i=0;i<(nums.size()-1);++i){
-            int max1=0;
-            int nextInd=-1;
-            for(int j=(i+1);j<=(nums[i]+i);++j){
-                if(j==(nums.size()-1)){
-                    return true;
-                }
-                if((nums[j]+j)>max1){
-                    cout<<j<<" "<<j<<" "<<nums[j]<<endl;
-                    max1=nums[j]+j;
-                    nextInd=j;
-                }
-            }
-            if(nextInd!=(-1)){
-                cout<<nextInd<<endl;
-                i=nextInd;
-                --i;
-            }
-            else{
-                return false;
-            }
-        }
-        return false;
+        return i>=(nums.size()-1);
     }
 };
+// [2,3,0,3,1,0,1]
